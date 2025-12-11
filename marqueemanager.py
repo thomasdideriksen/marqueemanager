2fd1d033bbfe0d8080d2f95f0a103a7ee7ebd9f9
Thomas Dideriksen <thomas@dideriksen.com>
2025-12-11 12:10:41 +0100

    Removed print statement as the plugin architecture for ES-DE appears to be buggy in its attempt to capture stdout(?)

--- marqueemanager.py
+++ marqueemanager.py
@@ -765,7 +765,6 @@ class VideoPlaybackEffect(Effect):
             if video_path != self.loaded_video_path:
                 # This is a new video, load it
                 self.cleanup()
-                print(f'Loading: {video_path}')
                 self.video, self.tex = self._load_video(renderer, video_path)
                 if self.video is None or self.tex is None:
                     if len(self.video_paths) == 1:
